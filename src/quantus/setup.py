00fd20d9b96060c5b2dbb09e7df8e2308e62639c
Andrew Trask <liamtrask@gmail.com>
2014-05-31 20:09:29 -0500

    playing with bootstrap code

--- src/quantus/setup.py
+++ src/quantus/setup.py
@@ -46,7 +46,7 @@ setup(name="quantus",
       cmdclass={'test': PyTest},
       
       # TODO: List of packages that this one depends upon:   
-      install_requires=['numpy', 'theano', 'zmq'],
+      install_requires=['numpy', 'theano', 'pyzmq'],
       # TODO: List executable scripts, provided by the package (this is just an example)
       entry_points={
         'console_scripts': 
