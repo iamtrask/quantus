c413110994396b7bc7267b3be1a3725b9af188d3
Andrew Trask <liamtrask@gmail.com>
2014-05-31 19:09:58 -0500

    added init and change file structure

--- src/main/com.quantus.master/controller.py
+++ src/main/com.quantus.master/controller.py
@@ -1,13 +1,13 @@
 import zmq
+
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 
 socket.connect("tcp://localhost:5555")
 
-while(1>0):
-	
-	variable = raw_input('quantus>')
-	socket.send(variable)
-	print(socket.recv())
+while (1 > 0):
+    variable = raw_input('quantus>')
+    socket.send(variable)
+    print(socket.recv())
 
 
