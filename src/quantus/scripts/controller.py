44201402a94d66f23c50dec8a52d56d2178c059b
Andrew Trask <liamtrask@gmail.com>
2014-05-31 19:40:24 -0500

    added quatus code back into better boilerplate

---
+++ src/quantus/scripts/controller.py
@@ -0,0 +1,14 @@
+__author__ = 'andrewtrask'
+
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+
+socket.connect("tcp://localhost:5555")
+
+while (1 > 0):
+    variable = raw_input('quantus>')
+    socket.send(variable)
+    print(socket.recv())
+
